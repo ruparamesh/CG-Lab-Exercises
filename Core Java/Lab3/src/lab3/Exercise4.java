12076c0f3918218498a8bad0172db73788cb1d02
ruparamesh <64540615+ruparamesh@users.noreply.github.com>
2021-03-11 19:08:31 +0530

    Update Exercise4.java

--- Core Java/Lab3/src/lab3/Exercise4.java
+++ Core Java/Lab3/src/lab3/Exercise4.java
@@ -1,17 +1,18 @@
 package lab3;
 
 public class Exercise4 {
-	public static void main(String args[]){ 
-		   int y=0, z=0;
-		   int x = Integer.parseInt(args[0]);
-		   while(x>0)
-		   {
-		    y=(x%10);
-		    x=x/10;
-		    y=y-(x%10);
-		    System.out.print(y);
-		    
-		   }
-		   
-		  }
+	public static int modifyNumber(int n) {
+		String[] str = String.valueOf(n).split("");
+		
+		return Integer.parseInt(str.toString());		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		int n = sc.nextInt();
+		
+		System.out.println(modifyNumber(n));
+		sc.close();
+	}
 }
